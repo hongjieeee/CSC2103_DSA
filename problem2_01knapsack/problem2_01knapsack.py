27ec7a5c9203657cc6b121d02eb9b01bde7db7d7
iZ86 <iZLikesCS@gmail.com>
2026-08-02 08:09:47 +0800

    Add problem2_01knapsack.py solution

---
+++ problem2_01knapsack/problem2_01knapsack.py
@@ -0,0 +1,23 @@
+
+
+def knapsack_tabulation():
+    n = len(values)
+    tab = [[0]*(capacity + 1) for y in range(n + 1)]
+
+    for i in range(1, n+1):
+        for w in range(1, capacity+1):
+            if weights[i-1] <= w:
+                include_item = values[i-1] + tab[i-1][w-weights[i-1]]
+                exclude_item = tab[i-1][w]
+                tab[i][w] = max(include_item, exclude_item)
+            else:
+             tab[i][w] = tab[i-1][w]
+
+    for row in tab:
+        print(row)
+    return tab[n][capacity]
+
+values = [300, 200, 400, 500]
+weights = [2, 1, 5, 3]
+capacity = 10
+print("\nMaximum value in Knapsack =", knapsack_tabulation())
